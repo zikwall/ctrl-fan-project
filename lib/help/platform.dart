265ad82f893a833eb0b46c49b11e382c5f9d1ba7
zikwall <andrey.kapitonov.96@gmail.com>
2021-03-13 17:34:06 +0300

    Add development mode and platform specific code

--- lib/help/platform.dart
+++ lib/help/platform.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io' show Platform;
 import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart' as Foundation;
 
@@ -10,6 +11,17 @@ class PlatformHelper {
   static Future<bool> isInstalledFromMarket() async {
     bool status = false;
 
+    // development mode is not installed from market
+    if (!isProd) {
+      return true;
+    }
+
+    // apple always install from market
+    if (Platform.isIOS) {
+      return true;
+    }
+
+    // check android
     try {
       status = await platform.invokeMethod('installedFromMarket');
     } on PlatformException catch (e) {
